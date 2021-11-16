aa81986f26217772dc27f37d24e19ecbf5c2b356
Leonardo Souza <leonardo.souza@care.com>
2021-11-16 15:12:34 +0100

    add intial code

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hellow world text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const hello = screen.getByText(/Hello world/i);
+  expect(hello).toBeInTheDocument();
 });
