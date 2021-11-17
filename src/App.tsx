e5d8abf3c7c1aff4078ae3989755fd28f7a961bd
Leonardo Souza <leonardo.souza@care.com>
2021-11-17 10:04:08 +0100

    some more adjusts to the basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import ClickMe from './components/ClickMe';
+import Weather from './Weather/Weather';
+import { Grid } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <br /><br />
         <p>Hello world</p>
-        <ClickMe />
       </header>
-    </div>
+      <Grid container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}>
+        <Grid item xs={6}>
+          <ClickMe />
+        </Grid>
+        <Grid item xs={6}>
+          <Weather name="berlin" isLoadingText={'loading ...'} />
+        </Grid>
+      </Grid>
+    </div >
   );
 }
 
